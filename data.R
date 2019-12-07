7ab239d192acb7a04052f9a62e1f0a2305074bdd
Lizette <lcarpenter@smith.edu>
2019-12-06 20:31:50 -0500

    Dropped three columns

--- data.R
+++ data.R
@@ -4,7 +4,8 @@ library(janitor)
 
 df <- ipums_time_used %>% 
   clean_names() %>%
-  filter(!is.na(wb_resp))
+  filter(!is.na(wb_resp)) %>%
+  select(-wt06, -pernum, -lineno)
 
 #Years in df include: 
 unique(df$year)
